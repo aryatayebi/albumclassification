ab261109ec8159c5ffadaa57284c120204f1085e
Arya Tayebi <atayeb@umich.edu>
2018-12-09 14:52:26 -0500

    updated torch in loadData

--- loadData.py
+++ loadData.py
@@ -1,4 +1,3 @@
-
 import os
 import pandas as pd
 import numpy as np
@@ -6,20 +5,29 @@ import json
 import shutil
 import requests
 import cv2
+import math
+import torch
+import torch.utils.data
+import torch.nn.functional as F
+import torch.optim as lfunc
 
-_API_ROOT = "http://ws.audioscrobbler.com/2.0/"
+_API_ROOT = "https://api.spotify.com/v1/search"
 _NUM_ROWS_TRAIN_PER_GENRE = 100
 
 class Dataset():
     """Dataset class for album classification"""
 
-    def __init__(self, param_1):
+    def __init__(self, param_1, debug=False):
         """  Loads data from last.fm API
-
             ARGS:
                 param_1: personal api_key to access last.fm API calls
-
         """
+
+        num_rows = range(0, 1000, 50)
+
+        if debug == True:
+            num_rows = range(1)
+
 
         self.api_key = param_1
 
@@ -27,27 +35,28 @@ class Dataset():
         album_genre = []
         album_image_url = []
         album_image = []
-        genre_list = ['electronic', 'indie', 'pop', 'metal', 'alternative rock', 'classic rock',
-                      'jazz', 'folk', 'punk', 'Hip-Hop', 'Classical', 'rap']
+        genre_list = ['electronic', 'indie', 'pop', 'metal', 'alternative%20rock', 'classic%20rock',
+                      'jazz', 'folk', 'rap', 'classical']
 
         for genre in genre_list:
-            response = requests.get(_API_ROOT + "?method=tag.gettopalbums&tag=" + genre +
-                                    "&limit=100&api_key="+ self.api_key + "&format=json")
-            data01 = response.json()
+            for i in num_rows:
+                response = requests.get(_API_ROOT + "?q=genre%3A" + genre + "&type=track&limit=50&offset=" + str(i), headers = {"Authorization": "Bearer " + self.api_key})
+                data01 = response.json()
+                print(i)
 
-            if response.status_code == 200:
-                for album in data01['albums']['album']:
-                    if 'name' in album and 'image' in album and not album['name'] == "" and not album['image'][3]['#text'] == "":
-                        album_name.append(album['name'])
-                        album_genre.append(genre)
-                        album_image_url.append(album['image'][3]['#text'])
-                        # print(album['name'])
-                        # print(album['image'][3]['#text'])
-            else:
-                print('Error requesting API for ' + genre)
+                if response.status_code == 200:
+                    for item in data01['tracks']['items']:
+                        if len(item['album']['images']) > 2:
+                            album_name.append(item['album']['name'])
+                            album_genre.append(genre)
+                            album_image_url.append(item['album']['images'][2]['url'])
 
-        for url in album_image_url:
-            print(url)
+                else:
+                    print('Error requesting API for ' + genre)
+                    exit(1)
+
+        for index, url in enumerate(album_image_url):
+            print("Getting URL " + str(index))
             img = requests.get(url, stream=True)
 
             if img.status_code == 200:
@@ -61,24 +70,115 @@ class Dataset():
 
             else:
                 print("Error loading image " + url)
+                exit(1)
 
-
-        self.train = pd.DataFrame(
+        # create data frame with all samples loaded
+        self.all_data = pd.DataFrame(
             {'name': album_name,
              'genre': album_genre,
              'image_url': album_image_url,
              'image': album_image
              })
 
+        # shuffle the data
+        # self.all_data = all_data.sample(frac=1).reset_index(drop=True)
+        # dataLen = self.all_data.shape[0]
+        # length80 = math.floor(dataLen * 0.8)
+        # length10 = math.floor(dataLen * 0.1)
+
+        # split data in train/validate/test with 80%/10%/10% of rows from all_data
+        # self.train = self.all_data.iloc[:length80]
+        # self.validate = self.all_data.iloc[length80:(length80 + length10)]
+        # self.test = self.all_data.iloc[(length80 + length10):]
+
+    def preprocessKNN(self):
+        """Processes data for kNN
+        Returns:
+            Train data, validate data, test data
+        """
+
+        feature_list = []
+
+        for index, row in self.all_data.iterrows():
+            chans = cv2.split(row['image'])
+
+            features = []
+            for chan in chans:
+                hist = cv2.calcHist(chan, [0], None, [64], [0,256])
+                features.extend(hist)
+
+            features = np.array(features).flatten()
+            feature_list.append(features)
+
+        df = self.all_data[['name', 'genre']].copy()
+
+        feature_df = pd.DataFrame(feature_list)
+
+        df = df.join(feature_df)
+
+        # dataLen = df.shape[0]
+        # length80 = math.floor(dataLen * 0.8)
+        # length10 = math.floor(dataLen * 0.1)
+
+        # return df.iloc[:length80], df.iloc[length80:(length80 + length10)], df.iloc[(length80 + length10):]
+
+        return df
+
+    def preprocessCNN(self):
+        labels = np.zeros(self.all_data.shape[0], dtype=int)
+        new_images = []
+
+        for index, row in self.all_data.iterrows():
+            genre = row['genre']
+
+            if (genre == 'electronic'):
+                labels[index] = 0
+
+            if (genre == 'indie'):
+                labels[index] = 1
+
+            if (genre == 'pop'):
+                labels[index] = 2
+
+            if (genre == 'metal'):
+                labels[index] = 3
+
+            if (genre == 'alternative rock'):
+                labels[index] = 4
+
+            if (genre == 'classic rock'):
+                labels[index] = 5
+
+            if (genre == 'jazz'):
+                labels[index] = 6
+
+            if (genre == 'folk'):
+                labels[index] = 7
+
+            if (genre == 'Hip-Hop'):
+                labels[index] = 8
+
+            if (genre == 'Classical'):
+                labels[index] = 9
+
+            img = (row['image'])
+            img2 = cv2.resize(img, (32, 32))
+            tp = np.transpose(img2)
+            new_images.append(tp)
+
+        new_images = np.array(new_images)
+        torch_images = torch.from_numpy(new_images)
+        torch_labels = torch.from_numpy(labels)
+        return torch.utils.data.TensorDataset(torch_images, torch_labels)
 
 
 
 if __name__=='__main__':
 
-    apiKey = "18a7c1e4adc3bc81521a35f3f4f3a7bf"
-    data = Dataset(apiKey)
+    apiKey = "BQC_ZHdMrIVsP8URqeqzWqdoQ5EBaTmDzI5XIqpPNIq3nxKhq_HE1mzcjaY6fv00vLC8W8C-35MrgjWx4aBzgwK2bRGcxL-6sHF7u3W9p7oC3nm9X4aeofuJEViQi3sMl3z3tFreaDaXSehXtMw"
+    debug = True
+    data = Dataset(apiKey, debug)
 
-    for index, row in data.train.iterrows():
-        print(row['name'] + "  " + row['genre'])
+    print(data.all_data)
 
     print("loadData is complete")
